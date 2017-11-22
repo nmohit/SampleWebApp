a1ef4901d85f2af45448f244314f87f6540bcbe4
nmohit <nmohit@users.noreply.github.com>
2017-11-22 13:52:45 -0600

    Add project files.

---
+++ SampleWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleWebApp.Startup))]
+namespace SampleWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
